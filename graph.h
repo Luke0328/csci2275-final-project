0fe1711711f2d4645c4d823318ff057b596e1732
Luke Pan <curly0328@gmail.com>
2020-12-01 18:40:41 -0700

    Header files

---
+++ graph.h
@@ -0,0 +1,40 @@
+// Luke Pan, Final Project, Dr. Rhonda Hoenigman
+
+#include<iostream>
+#include<vector>
+
+struct vertex;
+
+struct adjVertex{
+    vertex *v;
+    double weight;
+};
+
+struct vertex{
+    std::string city;
+    bool visited;
+    double distance;
+    std::vector<adjVertex> adj;
+};
+
+class Graph
+{
+    public:
+        Graph(){};  
+        ~Graph(){}; 
+        void addEdge(std::string v1, std::string v2, int weight);
+        void addVertex(std::string name);
+        void printGraph();
+        void assignGroupID();
+        vertex *findVertex(std::string name);
+        void bfs(std::string name);
+        void dijkstras(std::string name);
+        double getTotalDistance();
+        void distributeByPriority();
+        void distributeByJumps();
+
+    protected:
+    private:
+        std::vector<vertex> vertices;
+        double totalDistance;
+};
---
+++ priorityQueue.h
@@ -0,0 +1,25 @@
+// Luke Pan, Final Project, Dr. Rhonda Hoenigman
+
+#include <iostream>
+
+struct node{
+    std::string city;
+    int population;
+};
+
+class PriorityQH
+{
+public:
+    PriorityQH(int n);
+    void push(std::string n, int p, int t);
+    node pop();
+    void printQH();
+protected:
+private:
+    node *maxHeap;
+    int capacity;
+    int currSize;
+    void maxHeapify(int i);
+    void swap(int i, int j);
+};
+
